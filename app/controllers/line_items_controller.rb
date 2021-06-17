1c73701fb4a004cae8ea3de28019059666fc1c60
Shaher-11 <shahershamroukh@gmail.com>
2021-06-17 17:27:25 +0200

    Add and style adding proudcts

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,4 +1,6 @@
 class LineItemsController < ApplicationController
+  include CurrentCart
+  before_action :set_cart, only: [:create]
   before_action :set_line_item, only: %i[ show edit update destroy ]
 
   # GET /line_items or /line_items.json
@@ -20,20 +22,22 @@ class LineItemsController < ApplicationController
   end
 
   # POST /line_items or /line_items.json
-  def create
-    @line_item = LineItem.new(line_item_params)
-
-    respond_to do |format|
-      if @line_item.save
-        format.html { redirect_to @line_item, notice: "Line item was successfully created." }
-        format.json { render :show, status: :created, location: @line_item }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+    def create
+      product = Product.find(params[:product_id])
+      @line_item = @cart.line_items.build(product: product)
+      respond_to do |format|
+        if @line_item.save
+        format.html { redirect_to @line_item.cart,
+          notice: 'Line item was successfully created.' }
+        format.json { render :show,
+          status: :created, location: @line_item }
+        else
+          format.html { render :new }
+          format.json { render json: @line_item.errors,
+            status: :unprocessable_entity }
+        end
       end
     end
-  end
-
   # PATCH/PUT /line_items/1 or /line_items/1.json
   def update
     respond_to do |format|
