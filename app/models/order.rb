9a10509c5b5074582af34409491bea784f4affc3
Shaher-11 <shahershamroukh@gmail.com>
2021-06-19 12:27:06 +0200

    Style the payment form

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,3 @@
 class Order < ApplicationRecord
-  enum pay_type: {
-    "Check"
-    => 0,
-    "Credit card"
-    => 1,
-    "Purchase order" => 2
-    }
+  enum pay_type: { "Check" => 0, "Credit card" => 1, "Purchase order" => 2}
 end
