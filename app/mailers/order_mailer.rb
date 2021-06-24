12f0912209eeca0f19510bc37819eb7f2361f9e9
Shaher-11 <shahershamroukh@gmail.com>
2021-06-24 09:45:01 +0200

    Modify the method and template

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -5,11 +5,10 @@ class OrderMailer < ApplicationMailer
   #
   #   en.order_mailer.received.subject
   #
-  def received
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
-  end
+    def received(order)
+      @order = order
+      mail to: order.email, subject: 'Pragmatic Store Order Confirmation'
+    end
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
