b11cd630dcf2cbadf6d3d985808c3954a6e3f9a2
Shaher-11 <shahershamroukh@gmail.com>
2021-07-12 11:16:41 +0200

    Modify the react files

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+window.I18n = require("../../../public/javascripts/i18n")
+require("../../../public/javascripts/translations")
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.middleware.use I18n::JS::Middleware
+
 module Zapcart
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
