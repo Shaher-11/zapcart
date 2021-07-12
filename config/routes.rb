1fdda9e8421819fd3048b09e76e5c72838e2c3f7
Shaher-11 <shahershamroukh@gmail.com>
2021-07-12 10:09:18 +0200

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,10 @@ Rails.application.routes.draw do
   resources :products do
     get :who_bought, on: :member
   end
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
 end
